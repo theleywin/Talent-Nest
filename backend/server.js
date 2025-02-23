c37387d69c118bfb00bef57596cd51179965be12
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2025-02-22 20:58:29 -0500

    parses json data from http request

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
