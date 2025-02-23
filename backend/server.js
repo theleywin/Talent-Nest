7ed759f9b9b5f8bc5b0cc9f1a30b8e7cb85e0a3e
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2025-02-22 20:34:30 -0500

    bd connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
+
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+app.use("/api/v1/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
