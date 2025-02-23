4fadc05b6e569d0a59f92e9f867a7ae0a1fc7244
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2025-02-22 23:31:10 -0500

    routes protected

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import {login, logout, signup} from "../controllers/auth.controller.js";
+import {login, logout, signup, getCurrentUser} from "../controllers/auth.controller.js";
+import {protectRoute} from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.get("/me",protectRoute,getCurrentUser);
 export default router;
