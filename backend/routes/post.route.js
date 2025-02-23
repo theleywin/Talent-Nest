2ceb165e87200f519e6c8559a571df896f0ee34e
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2025-02-23 18:06:49 -0500

    first functions created

---
+++ backend/routes/post.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+    createPost,
+    getFeedPosts,
+    deletePost,
+    getPostById
+} from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getFeedPosts);
+router.post("/create", protectRoute, createPost);
+router.delete("/delete/:id", protectRoute, deletePost);
+router.get("/:id", protectRoute, getPostById);
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
+import postRoutes from './routes/post.route.js';
 import { connectDB } from './lib/db.js';
 
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/posts", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
