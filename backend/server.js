e44edabc2980d6a9b2cc3ca3c9c0bbef8fc5cc91
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2025-02-24 00:41:18 -0500

    feat/notifications-route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
+import notificationRoutes from "./routes/notification.route.js";
 import { connectDB } from './lib/db.js';
 
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
