67e50655f8426f6cca3d6374ac22cd6300d8437b
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2025-03-17 10:50:19 -0400

    add cors lib to backend to handle requests

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,19 @@ import postRoutes from './routes/post.route.js';
 import notificationRoutes from "./routes/notification.route.js";
 import connectionRoutes from "./routes/connection.route.js";
 import { connectDB } from './lib/db.js';
+import cors from 'cors';
 
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
